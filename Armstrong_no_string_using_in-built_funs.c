ed1ddf24a6256cf660373f16441b2282104ba5ed
Sirajudeen <35687352+akkssr@users.noreply.github.com>
2022-01-15 21:18:29 +0530

    Update Armstrong_no_string_using_in-built_funs.c

--- Armstrong_no_string_using_in-built_funs.c
+++ Armstrong_no_string_using_in-built_funs.c
@@ -1,15 +1,13 @@
 
 
-//    Armstrong number using in-built functions
+//    Armstrong number using in-built functions by siraj
 
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
-
-int main(){
-    
+void main(){
     char str[30];
     
     printf("Enter the number\n");
@@ -24,8 +22,5 @@ int main(){
         sum+=pow((str[i]-48),len);                                        // Armstrong number logic
     }
     
-    (sum==num)? printf("\nArmstrong No = %lld and %lld\n",sum,num) : printf("\nNot Armstrong = %lld and %lld\n",sum,num);
-    
-    return 0;
-    
+    (sum==num)? printf("\nGiven Number %lld is Armstrong Number\n",num) : printf("\nGiven Number %lld is not Armstrong Number\n",num);    
 }
